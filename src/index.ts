163f29fef55257fcc173cf0007f92607fb83265a
Ilyasov Viktor <ilasovviktor2666@gmail.com>
2024-10-02 06:24:04 +0300

    Feat: add class form

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import "./styles/styles.css"
 
 import { todos } from './utils/constants';
-import { Item } from './components/Item'
+import { Item } from './components/Item';
+import { Form } from './components/Form';
 
 const contentElement = document.querySelector('.todos__list');
 
 const template = document.querySelector('#todo-item-template') as HTMLTemplateElement;
+const formElement: HTMLFormElement = document.querySelector('.todos__form');
+
+function handleFormSubmit(data: string) {
+    const todoItem = new Item(template);
+    const itemElement = todoItem.render(data);
+    contentElement.prepend(itemElement);
+    todoForm.clearForm();
+}
+
+const todoForm = new Form(formElement, handleFormSubmit);
 
 todos.forEach( (item) => {
     const todoItem = new Item(template);
